38c2adf0a668fdf59f36933e480f53047f400326
Gaurav Agarwal <gauravagarwalr@gmail.com>
2015-08-30 12:45:08 +0530

    Working taskmaster.

--- taskmaster/main.go
+++ taskmaster/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+  "fmt"
+  "bytes"
+  "net/http"
+  "io/ioutil"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+  resp, _ := http.Post("http://192.168.0.35:8080/postcrc", "text/plain", bytes.NewBufferString("your string"))
+
+  htmlData, _ := ioutil.ReadAll(resp.Body)
+
+  fmt.Fprintf(w, "Posted data and received: %s", htmlData)
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+  http.HandleFunc("/", handler)
+  http.ListenAndServe(":8080", nil)
 }
