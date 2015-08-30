de1f409fbbd65e80a232ff5a5cf113f2a3517eb1
Gaurav Agarwal <gauravagarwalr@gmail.com>
2015-08-30 13:35:05 +0530

    Logging errors.

--- taskmaster/main.go
+++ taskmaster/main.go
@@ -5,13 +5,15 @@ import (
   "bytes"
   "net/http"
   "io/ioutil"
+  "log"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
   resp, _ := http.Post("http://192.168.0.35:8080/postcrc", "text/plain", bytes.NewBufferString("your string"))
 
-  htmlData, _ := ioutil.ReadAll(resp.Body)
+  htmlData, err := ioutil.ReadAll(resp.Body)
 
+  if err != nil { log.Print(err) }
   fmt.Fprintf(w, "Posted data and received: %s", htmlData)
 }
 
